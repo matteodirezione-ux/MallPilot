857c3bab14402b1f99cc8881de4135691bbe4e60
Matteo Trotta <matteo.direzione@gmail.com>
2026-02-18 14:02:47 +0000

    File changes

--- src/Layout.jsx
+++ src/Layout.jsx
@@ -18,6 +18,7 @@ import { Button } from '@/components/ui/button';
 
 export default function Layout({ children, currentPageName }) {
   const [user, setUser] = useState(null);
+  const [displayName, setDisplayName] = useState(null);
   const [sidebarOpen, setSidebarOpen] = useState(true);
   const [centri, setCentri] = useState([]);
   const [centroSelezionato, setCentroSelezionato] = useState(null);
