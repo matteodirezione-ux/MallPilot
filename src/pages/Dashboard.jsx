614b1a6ca97ab7e5575033d7812ffc9c743329bb
Matteo Trotta <matteo.direzione@gmail.com>
2026-02-18 11:33:08 +0000

    File changes

--- src/pages/Dashboard.jsx
+++ src/pages/Dashboard.jsx
@@ -305,7 +305,7 @@ export default function Dashboard({ centroSelezionato }) {
 
 
         {/* Affitti Correnti */}
-        <Card className="md:col-span-2 lg:col-span-6 bg-white border-slate-200 hover:shadow-lg transition-shadow">
+        <Card className="md:col-span-2 lg:col-span-2 bg-white border-slate-200 hover:shadow-lg transition-shadow">
           <CardHeader>
             <div className="flex items-center gap-2">
               <TrendingUp className="w-5 h-5 text-green-600" />
